1b716d503ceaef6ae8f2a9062a81152bbe595ff1
meet <omar19@meet.mit.edu>
2018-08-08 07:10:59 -0400

    lol

--- lab/app.py
+++ lab/app.py
@@ -8,6 +8,6 @@ def home():
 
 @app.route('/student/<int:student_id>')
 def display_student(student_id):
-    return render_template('student.html', student_id=student_id)
+    return render_template('student.html', student_id=student_id, student=query_by_id(student_id) )
 
 app.run(debug=True)
